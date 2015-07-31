5c71469e67d83c82bc35a2db88e1e405864203d9
Ryan Smith <ryan@smithx.net>
2015-07-30 21:10:36 -0700

    adding Mail.app compose new mail project

---
+++ Projects/MailAppTransition/MailAppTransition.framer/app.coffee
@@ -0,0 +1,99 @@
+# Mail.app Compose New Mail Transition
+# Ryan Smith | Smithx.net | @byryan
+
+# Set Default Animation
+Framer.Defaults.Animation = {
+	curve: "spring(150, 22, 0)"
+	time: 0.4
+}
+
+###
+Layers
+###
+
+# Set Blured Background Layer
+backgroundLayer = new Layer
+	width: Screen.width, height: Screen.height
+	image: "images/background.png"
+
+# Inbox View
+inboxView = new Layer
+	width: Screen.width, height: Screen.height
+	image: "images/inboxView.png"
+
+# Status Bars
+statusBarBlack = new Layer
+	width: Screen.width
+	height: 40
+	image: "images/statusBarBlack.png"
+statusBarWhite = new Layer
+	width: Screen.width
+	height: 40
+	image: "images/statusBarWhite.png"
+	opacity: 0
+
+# Tap Area for Compose Icon
+composeTapArea = new Layer
+	maxX: Screen.width, maxY: Screen.height
+	width: 112,	height: 96
+	opacity: 0
+	superLayer: inboxView
+
+# Compose View
+composeView = new Layer
+	y: Screen.height
+	width: Screen.width, height: 1254
+	image: "images/composeView.png"
+
+# Tap Area for Cancel Icon
+cancelTapArea = new Layer
+	width: 200, height: 100
+	opacity: 0
+	superLayer: composeView
+
+###
+States
+###
+
+# toggle black status bar
+statusBarBlack.states.add({
+	on: { opacity: 1 },
+	off: { opacity: 0 },
+})
+
+# toggle white status bar
+statusBarWhite.states.add({
+	on: { opacity: 1 },
+	off: { opacity: 0 },
+})
+
+# scale inbox view
+inboxView.states.add({
+	default: { scale: 1 },
+	scaled: { scale: 0.92, maxY: Screen.height },
+})
+
+# reposition compose view
+composeView.states.add({
+	default: { y: Screen.height },
+	active: { y: 72 },
+})
+
+
+###
+Events
+###
+
+# Tap Compose Icon
+composeTapArea.on Events.Click, ->
+	inboxView.states.switch("scaled")
+	composeView.states.switch("active")
+	statusBarBlack.states.switch("off")
+	statusBarWhite.states.switch("on")
+
+# Tap Cancel Button
+cancelTapArea.on Events.Click, ->
+	inboxView.states.switch("default")
+	composeView.states.switch("default")
+	statusBarBlack.states.switch("on")
+	statusBarWhite.states.switch("off")
---
+++ Projects/MailAppTransition/MailAppTransition.framer/imported/viewScale/layers.json.js
@@ -0,0 +1,387 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["viewScale/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "89CAD2EE-776C-4143-93DE-A243EC1C3C30",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "53756B9E-83CC-4FB1-80A3-5C549AF055DF",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-navbar-53756B9E-83CC-4FB1-80A3-5C549AF055DF.png",
+              "frame" : {
+                "y" : 0,
+                "x" : 0,
+                "width" : 750,
+                "height" : 129
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 0,
+              "x" : 0,
+              "width" : 750,
+              "height" : 129
+            },
+            "name" : "navbar"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "24065864-755D-4169-92AA-657FE1A7F539",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-searchField-24065864-755D-4169-92AA-657FE1A7F539.png",
+              "frame" : {
+                "y" : 128,
+                "x" : 0,
+                "width" : 750,
+                "height" : 88
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 128,
+              "x" : 0,
+              "width" : 750,
+              "height" : 88
+            },
+            "name" : "searchField"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "09CB6C4E-78A1-4D63-8CAE-A821294C98A1",
+            "visible" : true,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "19CD8C3C-33B3-4E02-B1B2-F175C4265BFB",
+                "visible" : true,
+                "children" : [
+                  {
+                    "maskFrame" : null,
+                    "id" : "E64C6D17-A6C8-4BE4-8F4D-E94B9708774E",
+                    "visible" : true,
+                    "children" : [
+
+                    ],
+                    "image" : {
+                      "path" : "images\/Layer-pencil-E64C6D17-A6C8-4BE4-8F4D-E94B9708774E.png",
+                      "frame" : {
+                        "y" : 1267,
+                        "x" : 697,
+                        "width" : 26,
+                        "height" : 26
+                      }
+                    },
+                    "imageType" : "png",
+                    "layerFrame" : {
+                      "y" : 1267,
+                      "x" : 697,
+                      "width" : 26,
+                      "height" : 26
+                    },
+                    "name" : "pencil"
+                  }
+                ],
+                "image" : {
+                  "path" : "images\/Layer-composeIcon-19CD8C3C-33B3-4E02-B1B2-F175C4265BFB.png",
+                  "frame" : {
+                    "y" : 1267,
+                    "x" : 681,
+                    "width" : 42,
+                    "height" : 42
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 1267,
+                  "x" : 681,
+                  "width" : 42,
+                  "height" : 42
+                },
+                "name" : "composeIcon"
+              }
+            ],
+            "image" : {
+              "path" : "images\/Layer-toolbar-09CB6C4E-78A1-4D63-8CAE-A821294C98A1.png",
+              "frame" : {
+                "y" : 1245,
+                "x" : 0,
+                "width" : 750,
+                "height" : 89
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 1245,
+              "x" : 0,
+              "width" : 750,
+              "height" : 89
+            },
+            "name" : "toolbar"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "073DC1C3-5152-4FAA-A906-ADCA90ECE23F",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-firstCell-073DC1C3-5152-4FAA-A906-ADCA90ECE23F.png",
+              "frame" : {
+                "y" : 216,
+                "x" : 56,
+                "width" : 694,
+                "height" : 107
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 216,
+              "x" : 56,
+              "width" : 694,
+              "height" : 107
+            },
+            "name" : "firstCell"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "2D4BB1E1-41D4-4D18-BBF8-3982368C58CF",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-remainingCells-2D4BB1E1-41D4-4D18-BBF8-3982368C58CF.png",
+              "frame" : {
+                "y" : 323,
+                "x" : 56,
+                "width" : 694,
+                "height" : 963
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 323,
+              "x" : 56,
+              "width" : 694,
+              "height" : 963
+            },
+            "name" : "remainingCells"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-inboxView-89CAD2EE-776C-4143-93DE-A243EC1C3C30.png",
+          "frame" : {
+            "y" : 0,
+            "x" : 0,
+            "width" : 750,
+            "height" : 1334
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 0,
+          "x" : 0,
+          "width" : 750,
+          "height" : 1334
+        },
+        "name" : "inboxView"
+      }
+    ],
+    "id" : "5535440D-8FB3-4202-A9A1-30534CED4443",
+    "visible" : true,
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 750,
+      "height" : 1334
+    },
+    "name" : "Inbox"
+  },
+  {
+    "maskFrame" : null,
+    "id" : "A8D0A025-951D-47B1-806F-FAC29B794674",
+    "visible" : false,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "82BA1FE6-90EC-4C81-BE4F-9BA3E78BFB8F",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-Status_Bar_Black-82BA1FE6-90EC-4C81-BE4F-9BA3E78BFB8F.png",
+          "frame" : {
+            "y" : 51,
+            "x" : 13,
+            "width" : 724,
+            "height" : 20
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 51,
+          "x" : 13,
+          "width" : 724,
+          "height" : 20
+        },
+        "name" : "Status_Bar_Black"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "49F8C7B7-BE96-4341-BFC9-593D5EB3954C",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-Status_Bar_White-49F8C7B7-BE96-4341-BFC9-593D5EB3954C.png",
+          "frame" : {
+            "y" : 111,
+            "x" : 13,
+            "width" : 724,
+            "height" : 20
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 111,
+          "x" : 13,
+          "width" : 724,
+          "height" : 20
+        },
+        "name" : "Status_Bar_White"
+      }
+    ],
+    "image" : {
+      "path" : "images\/Layer-Status_Bars-A8D0A025-951D-47B1-806F-FAC29B794674.png",
+      "frame" : {
+        "y" : 0,
+        "x" : 0,
+        "width" : 750,
+        "height" : 180
+      }
+    },
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 750,
+      "height" : 180
+    },
+    "name" : "Status_Bars"
+  },
+  {
+    "maskFrame" : null,
+    "id" : "5D86ECA1-1E47-42AA-BC41-46C6BCDCE94D",
+    "visible" : false,
+    "children" : [
+
+    ],
+    "image" : {
+      "path" : "images\/Layer-background-5D86ECA1-1E47-42AA-BC41-46C6BCDCE94D.png",
+      "frame" : {
+        "y" : 0,
+        "x" : 0,
+        "width" : 750,
+        "height" : 1334
+      }
+    },
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 750,
+      "height" : 1334
+    },
+    "name" : "background"
+  },
+  {
+    "maskFrame" : null,
+    "id" : "2F7C8C0F-E3EC-41E0-89CC-762994455B25",
+    "visible" : false,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "72B20A8D-6720-4DB1-935F-725B65BB678F",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "9D5AF6B4-1EC9-4294-962D-6A05557D132A",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-addIcon-9D5AF6B4-1EC9-4294-962D-6A05557D132A.png",
+              "frame" : {
+                "y" : 192,
+                "x" : 676,
+                "width" : 44,
+                "height" : 44
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 192,
+              "x" : 676,
+              "width" : 44,
+              "height" : 44
+            },
+            "name" : "addIcon"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-composeView-72B20A8D-6720-4DB1-935F-725B65BB678F.png",
+          "frame" : {
+            "y" : 80,
+            "x" : 0,
+            "width" : 750,
+            "height" : 1254
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 80,
+          "x" : 0,
+          "width" : 750,
+          "height" : 1254
+        },
+        "name" : "composeView"
+      }
+    ],
+    "image" : {
+      "path" : "images\/Layer-composeView-2F7C8C0F-E3EC-41E0-89CC-762994455B25.png",
+      "frame" : {
+        "y" : 0,
+        "x" : 0,
+        "width" : 750,
+        "height" : 1334
+      }
+    },
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 750,
+      "height" : 1334
+    },
+    "name" : "composeView"
+  }
+]
